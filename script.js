d3d746a2aa8334e7a523f5b14d1765d682b54183
Glitch (hello-webpage) <none>
2020-01-07 18:22:16 +0000

    😹🐄 Checkpoint ./script.js:3093727/53 ./style.css:3093727/444 ./index.html:3093727/2209

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ var a = 0;
 function changegrid() {
   if (a == 0){
     document.getElementById("grid").className = "grid2";
+    console.log(a);
     a = 1;
-  }
-  if (a == 1){
+  } else if (a == 1){
     document.getElementById("grid").className = "grid";
+    console.log(a);
     a = 0;
   }
 }
