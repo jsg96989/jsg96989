46aa9586c332a0e18410f415871c7e549041c885
Glitch (hello-webpage) <none>
2020-01-10 14:47:30 +0000

    🌌⚾️ Checkpoint ./previousEpisodes.html:3093727/12120 ./index.html:3093727/93 ./tMPscript.js:3093727/83 ./pEscript.js:3093727/312

---
+++ pEscript.js
@@ -0,0 +1,20 @@
+var dates = [
+  "1/10/2020",
+  "1/11/2020",
+  "1/12/2020",
+  "1/13/2020",
+];
+
+var links = [
+  "www.google.com",
+  "www.google.com",
+  "www.google.com",
+  "www.google.com",
+];
+
+
+function loadPreviousEpisodes() {
+  
+}
+
+loadPreviousEpisodes();
